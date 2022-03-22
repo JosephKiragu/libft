a3492e4954bd7cd4a39f654b62613aa7b170681c
joseph kiragu <josephkiragu@josephs-MacBook-Air.local>
2022-03-22 21:56:49 +1030

    Adding version 1 bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -13,18 +13,6 @@
 
 void    ft_bzero(void *s1, size_t n) // call memset as  function body!
 {
-    unsigned char *s1Ptr;
-    size_t j;
+    ft_memset((void *)s1, 0, (size_t) n);
 
-    s1Ptr = (unsigned char *)s1;
-    j = 0;
-
-    if(!s1)
-        return;
-
-    while (j < n)
-    {
-       s1Ptr[j] = '\0'; //Use single quotes  to assign characters and Use double quotes  when assigning to the whole string array 
-    }
-    s1 = s1Ptr;
 }
