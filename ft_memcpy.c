1265429bce881c5409075707e8cf7763054f2ba3
joseph kiragu <josephkiragu@josephs-MacBook-Air.local>
2022-03-26 22:37:04 +1030

    Adding memmove and memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -15,16 +15,16 @@
 
 void    *ft_memcpy(void *dst, const void *src, size_t n)
 {
-    size_t i ;
+    char *dstPtr;
 
-    i = 0;
+    dstPtr = (char *)dst;
 
-    while (i < n)
+    while (n > 0)
     {
-        *((char *)(dst) + i) = (*(const char *)(src) + i);
-        i++;
+        *(char *)(dst)++ = *(const char *)(src)++;
+       n--;
     }
 
-    return dst;
+    return dstPtr;
 
 }
