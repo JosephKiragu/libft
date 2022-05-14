55ed6c9b1fdcb9faba15e15d58d2409d45613279
joseph kiragu <josephkiragu@josephs-MacBook-Air.local>
2022-05-14 16:31:57 +0930

    Changes to ft_lstlast.c

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -2,12 +2,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list	*temp;
-
-	temp = lst;
-	while (temp->next)
-	{
-		temp = temp->next;
-	}
-	return (temp);
+	if (!lst)
+		return (lst);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
